ec7c75080ab3dfa269335aeb310db36e80385196
VijaySamant4368 <vijaysamant4368@gmail.com>
2025-10-20 23:27:53 +0530

    Revert "Make routes and controllers"
    This reverts commit a981949821cf98d09958993eae3a89a279437cd0.

--- routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { login, verify } from '../controllers/authController.js';
-import { protect } from '../middleware/auth.js';
-
-const r = Router();
-r.post('/login', login);
-r.get('/verify', protect, verify);
-export default r;
